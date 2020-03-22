71401f235fc544f85d52d3d2d46f2c17ac9b7f74
Julien Pignolet <julien.pignolet@hotmail.fr>
2020-03-22 11:39:00 +0100

    Commment noteservice

--- src/main/java/JeuApplication/service/NoteService.java
+++ src/main/java/JeuApplication/service/NoteService.java
@@ -4,6 +4,11 @@ import JeuApplication.entity.Jeu;
 import JeuApplication.entity.Note;
 
 public class NoteService {
+    /***
+     * Calcul de la note
+     * @param reviews review d'un testeur
+     * @return Note pondérée
+     */
     public static double getNote(Note... reviews) {
         int cumulNote = 0;
         for (Note review : reviews)
@@ -12,26 +17,51 @@ public class NoteService {
         return cumulNote / reviews.length;
     }
 
+    /**
+     * Prise en compte de la notation de Didier Loyal
+     * @param review
+     * @return modification de note
+     */
     private static int updateNoteDidier(Note review) {
         return review.getJeu().getTheme().getNom_theme().equals("science-fiction")
                 && review.getNom_testeur().equals("Didier Loyal") && review.getNote() <= 9 ? 1 : 0;
     }
 
+    /**
+     * Prise en compte de la notation de Armande Moly
+     * @param review
+     * @return modification de note
+     */
     private static int updateNoteArmande(Note review) {
         return review.getJeu().getGenre().getNom_genre().equals("gestion")
                 && review.getNom_testeur().equals("Armande Moly") && review.getNote() < 10 ? 1 : 0;
     }
 
+    /**
+     * Prise en compte de la notation de Gaston Portaleau
+     * @param review
+     * @return modification de note
+     */
     private static int updateNoteGaston(Note review) {
         return review.getJeu().getType().getNom_type().equals("jeu de cartes")
                 && review.getNom_testeur().equals("Gaston Portaleau") && review.getNote() > 0 ? -1 : 0;
     }
 
+    /**
+     * Prise en compte de la notation de Liz Smallhead
+     * @param review
+     * @return modification de note
+     */
     private static int updateNoteLiz(Note review) {
         return review.getJeu().getEditeur().getNom_editeur().equals("édijeu")
                 && review.getNom_testeur().equals("Liz Smallhead") && review.getNote() >= 2 ? -2 : 0;
     }
 
+    /**
+     * Prise en compte de la notation de Stefan Bergor
+     * @param review
+     * @return modification de note
+     */
     private static int updateNoteStefan(Note review) {
         return review.getJeu().getTheme().getNom_theme().equals("contemporain")
                 && review.getNom_testeur().equals("Stefan Bergor") && review.getNote() >= 3 && review.getNote() <= 7 ? 1/2 : 0;
